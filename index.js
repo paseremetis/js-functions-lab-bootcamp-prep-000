a379254571668f6b9bea82270e6cbffb68e9f779
Peter Seremetis <pswriter5348@gmail.com>
2018-01-15 23:41:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// write your code below!
+function happyHolidays() {
+  var happy = "Happy holidays!"
+  return happy
+}
+function happyHolidaysTo(name) {
+  var name2 = "Joe"
+  var holiday = `Happy holidays, ${name}!`
+  var you = `Happy holidays, you!`
+  if (name==="Joe") {
+  return holiday; 
+  } else return you
+}
+function happyHolidayTo(holiday, name) {
+  var day = "Independence Day"
+  var name2 = "Joe"
+  var you = `Happy Independence Day, you!`
+  var statement = `Happy ${holiday}, ${name}!`
+  if (name==="Joe") {
+    return statement
+      else return you 
+  }
+  }
